63281cda9b32840440a9bf04bb4a1b07da58c7da
Alexander Koltun <alexander.koltun@gmail.com>
2016-03-27 22:55:39 +0200

    Add ui-router on the upper level

--- components/app/app.js
+++
@@ -1,3 +0,0 @@
-angular.module('mailApp').component('app', {
-  templateUrl: "components/app/app.html"
-});
--- init.js
+++ init.js
@@ -1,1 +1,12 @@
-angular.module("mailApp", []);
+angular.module("mailApp", ["ui.router"])
+.config(function($stateProvider) {
+	$stateProvider
+		.state('mail', {
+			url: '/mail',
+			template: '<mails></mails>'
+		})
+		.state('contacts', {
+			url: '/contacts',
+			template: '<contacts></contacts>'
+		});
+});
